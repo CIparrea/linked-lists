58dab10b2824cfe0a4a71462d196b16f0d9ee0ad
Cesar Iparrea <cesariparrea@hotmail.com>
2024-03-14 10:32:57 -0400

    Put request

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 class Node {
     constructor(val){
-        this.val = val,
+        this.val = val;
         this.next = null
     }
 }
+ 
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.lenght = 0;
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else{
+            this.tail.next = newNode
+            this.tail = newNode 
+        }
+
+        this.lenght++
+        return this
+    }
+}
+
+const list = new SinglyLinkedList()
+list.push("A")
